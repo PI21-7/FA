ccd5b5f1332cd466a9042ebfb69c9dbbd587dd26
Nikolai <ya.pikus@gmail.com>
2022-03-09 09:34:54 +0300

    Database update #6

--- Course_1/Telegram_Bot/db.py
+++ Course_1/Telegram_Bot/db.py
@@ -34,6 +34,24 @@ class Database(object):
     @Connections.safe
     def add_message(self, connection, subject_name: str, text: str, date: str) -> None:
         connection, cursor = connection
+        subject_name = subject_name.lower()
         # Записываем
         cursor.execute('''INSERT INTO Homework (subject_id, date, text) VALUES (?, ?, ?)''', (subject_name, date, text))
         connection.commit()
+
+    @Connections.safe
+    def receive_homework(self, connection, subject_name: str, date: str) -> str:
+        connection, cursor = connection
+        subject_name = subject_name.lower()
+        answer = cursor.execute(
+            '''SELECT text FROM Homework WHERE subject_id = ? and date = ? ''',
+            (subject_name, date))
+        connection.commit()
+
+        return answer.fetchall()[0][0]
+
+
+if __name__ == '__main__':
+    DB = Database()
+    DB.init()
+    print(DB.receive_homework(subject_name='Алгебра', date='08.03.22'))
