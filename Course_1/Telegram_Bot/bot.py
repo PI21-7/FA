ca271c7ed0a996d21d6cdbe0ebb464c894a9bf28
GinKey <monotank23@gmail.com>
2022-03-10 16:37:30 +0300

    dates update #8

--- Course_1/Telegram_Bot/bot.py
+++ Course_1/Telegram_Bot/bot.py
@@ -13,7 +13,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 storage = MemoryStorage()
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot, storage = storage)
+dp = Dispatcher(bot, storage=storage)
 db = db.Database()
 
 @dp.message_handler(commands=['start'], state="*")
@@ -21,42 +21,34 @@ async def process_start_command(message: types.Message):
 	await message.answer("Привет! Нажми на кнопку чтобы получить домашнее задание.", reply_markup=Buttons.answer_start)
 
 
-@dp.message_handler(lambda message: message.text == 'Получить задание!',  state = "*")
+@dp.message_handler(lambda message: message.text == 'Получить задание!',  state="*")
 async def process_date(message: types.Message, state: FSMContext):
 	await state.finish()
 	async with state.proxy() as data:
 		data['date_count'] = 0
-	await message.answer(f"*Выбираем дату \n{date.week_definition(0)[0]} {date.week_definition(0)[1]}*", parse_mode="markdown", reply_markup=Buttons.Inline_Date)
+	await message.answer(f"*Выбираем дату \n{date.week_definition(0)[0]} - {date.week_definition(0)[1]}*", parse_mode="markdown", reply_markup=Buttons.Inline_Date)
 
 
-@dp.callback_query_handler(text = "Inline_Date_Down")
+@dp.callback_query_handler(text="Inline_Date_Down")
 async def callback(call: types.CallbackQuery, state: FSMContext):
 	async with state.proxy() as data:
 		data['date_count'] += 1
 		date_count = data['date_count']
 
 
-	await bot.edit_message_text(chat_id = call.message.chat.id, message_id = call.message.message_id, 
-		text = f"*Выбираем дату \n{date.week_definition(date_count)[0]} {date.week_definition(date_count)[1]}*", parse_mode="markdown",  reply_markup=Buttons.Inline_Date)
-
-	async with state.proxy() as data:
-		if date.week_definition(date_count)[2] == False:
-			data['date_count'] -= 1
+	await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, 
+		text = f"*Выбираем дату \n{date.week_definition(date_count)[0]} - {date.week_definition(date_count)[1]}*", parse_mode="markdown",  reply_markup=Buttons.Inline_Date)
 
 
-@dp.callback_query_handler(text = "Inline_Date_Up")
+@dp.callback_query_handler(text="Inline_Date_Up")
 async def callback(call: types.CallbackQuery, state: FSMContext):
 	async with state.proxy() as data:
 		data['date_count'] -= 1
 		date_count = data['date_count']
 
 
-	await bot.edit_message_text(chat_id = call.message.chat.id, message_id = call.message.message_id,
-		text = f"*Выбираем дату \n{date.week_definition(date_count)[0]} {date.week_definition(date_count)[1]}*", parse_mode="markdown",  reply_markup=Buttons.Inline_Date)
-	
-	async with state.proxy() as data:
-		if date.week_definition(date_count)[2] == False:
-			data['date_count'] += 1
+	await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+		text = f"*Выбираем дату \n{date.week_definition(date_count)[0]} - {date.week_definition(date_count)[1]}*", parse_mode="markdown",  reply_markup=Buttons.Inline_Date)
 
 
 
--- Course_1/Telegram_Bot/date.py
+++ Course_1/Telegram_Bot/date.py
@@ -1,32 +1,10 @@
-import calendar
 import datetime
 
 def week_definition(count):
-    current_date = datetime.date.today()
-    week_day = current_date.weekday()
-    month_dates = list(calendar.Calendar().itermonthdays3(current_date.year, current_date.month))
-    day_index = month_dates.index((current_date.year, current_date.month, current_date.day))
-    if day_index - week_day + 6 + count*7 <= len(month_dates) and (month_dates[day_index - week_day + 6 + count*7] != month_dates[-1] and month_dates[day_index - week_day + count*7] != month_dates[0]):
-        
-        week_dates = month_dates[day_index - week_day + count*7], month_dates[day_index - week_day + 6 + count*7]
-
-        start_day = datetime.date(week_dates[0][0], week_dates[0][1], week_dates[0][2])
-        end_day = datetime.date(week_dates[1][0], week_dates[1][1], week_dates[1][2])
-        start_day = start_day.strftime("%d.%m.%Y")
-        end_day = end_day.strftime("%d.%m.%Y")
-
-        return f"{start_day} - ", end_day, True
-        
-    else:
-    	if count > 0:
-    		start_day = datetime.date(month_dates[-7][0], month_dates[-7][1], month_dates[-7][2])
-    		end_day = datetime.date(month_dates[-1][0], month_dates[-1][1], month_dates[-1][2])
-    		start_day = start_day.strftime("%d.%m.%Y")
-    		end_day = end_day.strftime("%d.%m.%Y")
-    		return "конец", f"месяца!\n{start_day} - {end_day}", False
-    	else:
-        	start_day = datetime.date(month_dates[0][0], month_dates[0][1], month_dates[0][2])
-        	end_day = datetime.date(month_dates[6][0], month_dates[6][1], month_dates[6][2])
-        	start_day = start_day.strftime("%d.%m.%Y")
-        	end_day = end_day.strftime("%d.%m.%Y")
-        	return "конец месяца!", f"\n{start_day} - {end_day}", False
+    current_date  = datetime.date.today()
+    week_day = datetime.timedelta(days=current_date.weekday())
+    end_week = datetime.timedelta(days=6)
+    week = datetime.timedelta(weeks=1)
+    start_date = current_date - week_day + count * week
+    end_date = current_date - week_day + end_week + count * week
+    return start_date.strftime("%d.%m.%y"), end_date.strftime("%d.%m.%y")
