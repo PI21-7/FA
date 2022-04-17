c003a572c4d92629d0cb0a8a8eb556ba260494a9
Nikolai <ya.pikus@gmail.com>
2022-04-17 15:56:49 +0300

    Bug fix 3.24

--- Course_1/Telegram_Bot/Buttons/__modules__.py
+++ Course_1/Telegram_Bot/Buttons/__modules__.py
@@ -7,8 +7,7 @@ from aiogram.utils.exceptions import MessageToDeleteNotFound
 from transliterate import translit
 from Buttons.Exactly_Buttons.Buttons import *
 from Utils.Groups import *
-from Utils.Miscellaneous import bot, HDB, get_user_group, days_of_week, days, SelfState, \
-	get_group_schedule, green_list
+from Utils.Miscellaneous import *
 from Utils.date import week_definition
 from Utils.debug import Debugger
 
