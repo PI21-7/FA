ae4fcf4c8aa402db1d8ada3f6df147e03b793bb0
Nikolai <ya.pikus@gmail.com>
2022-04-25 16:47:29 +0300

    рил сок обнова, ещё и документацию переписал, гений v 3.52

--- Course_1/Telegram_Bot/create_config.bat
+++ Course_1/Telegram_Bot/create_config.bat
@@ -4,9 +4,15 @@ chcp 65001
 
 set /p TOKEN="TOKEN: "
 
+set /p ADMIN_BOT="Admin_bot_token: "
+
 set /p Admin="Admin: "
  
 echo TOKEN = r'%TOKEN%' > config.py
+
+echo ADMIN_BOT_TOKEN = r'%ADMIN_BOT%' >> config.py
+
+echo ADMIN = None  # Admin's user_id >> config.py
 
 mkdir Admin
 
